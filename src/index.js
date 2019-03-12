f33b7c5d0ca04d8e9ea23e34f48c8b002a977f70
Mishkovec <neilsa@mail.ru>
2019-03-12 21:15:38 +0300

    bugs: remove if in for

--- src/index.js
+++ src/index.js
@@ -20,44 +20,15 @@ module.exports = function check(str, bracketsConfig) {
     
     var newStr=strOpen+strClose;
    // alert(newStr);
-    if(newStr.length>8){
-     let stack=[];
+    let stack=[];
     for (let i=0;i<str.length;i++){
        for(let j=0;j<newStr.length/2;j++){
            //alert(newStr[j]+''+newStr[j+newStr.length/2])
           if(str[i]==newStr[j]){
               stack.push(str[i]);
-             
-          } 
-         
-          if (str[i]==newStr[j+newStr.length/2]){
-             for(let k=0;k<newStr.length/2;k++){
-               if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
-                  //alert(str[i-1]+''+str[i]); 
+              /*if(newStr[j]==newStr[j+newStr.length/2]){
                   stack.pop();
-               } 
-             }
-          }
-       }
-    }
-    if(stack.length>0){
-        return false;
-    }
-    else { return true;
-        
-    }
-       
-    }
-    if(newStr.length<8){
-     let stack=[];
-    for (let i=0;i<str.length;i++){
-       for(let j=0;j<newStr.length/2;j++){
-           //alert(newStr[j]+''+newStr[j+newStr.length/2])
-          if(str[i]==newStr[j]){
-              stack.push(str[i]);
-              if(newStr[j]==newStr[j+newStr.length/2]){
-                  stack.pop();
-              }
+              }*/
               
           } 
          if (str[i]!==newStr[j]){
@@ -83,7 +54,5 @@ module.exports = function check(str, bracketsConfig) {
     }
     else { return true;
         
-    }
-       
     }
 }
