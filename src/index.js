56e04cdfe3d0976732905438aa1eb5583e33548d
Mishkovec <neilsa@mail.ru>
2019-03-12 21:07:35 +0300

    bugs: remove if in for

--- src/index.js
+++ src/index.js
@@ -20,8 +20,36 @@ module.exports = function check(str, bracketsConfig) {
     
     var newStr=strOpen+strClose;
    // alert(newStr);
-    
-     var stack=[];
+    if(newStr.length>8){
+       let stack=[];
+    for (let i=0;i<str.length;i++){
+       for(let j=0;j<newStr.length/2;j++){
+           //alert(newStr[j]+''+newStr[j+newStr.length/2])
+          if(str[i]==newStr[j]){
+              stack.push(str[i]);
+                           
+          }
+        if (str[i]!==newStr[j]){
+             for(let k=0;k<newStr.length/2;k++){
+               if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
+                  //alert(str[i-1]+''+str[i]); 
+                  stack.pop();
+               } 
+             }
+          }
+         if (str[i]==newStr[j+newStr.length/2]){
+             for(let k=0;k<newStr.length/2;k++){
+               if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
+                  //alert(str[i-1]+''+str[i]); 
+                  stack.pop();
+               } 
+             }
+          }
+       }
+    } 
+    }
+    if(newStr.length<8){
+      let stack=[];
     for (let i=0;i<str.length;i++){
        for(let j=0;j<newStr.length/2;j++){
            //alert(newStr[j]+''+newStr[j+newStr.length/2])
@@ -49,7 +77,9 @@ module.exports = function check(str, bracketsConfig) {
              }
           }
        }
+    }  
     }
+     
        
     
     //alert(stack);
