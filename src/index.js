2704852f4a37b8b526861a642da7c9705944ea45
Mishkovec <neilsa@mail.ru>
2019-03-13 20:13:09 +0300

    feat: add solve number brackets

--- src/index.js
+++ src/index.js
@@ -19,27 +19,18 @@ module.exports = function check(str, bracketsConfig) {
     
     
     var newStr=strOpen+strClose;
-   // alert(newStr);
-    let stack=[];
+   //alert(newStr.length);
+   if(newStr.length>8){
+     let stack=[];
     for (let i=0;i<str.length;i++){
        for(let j=0;j<newStr.length/2;j++){
            //alert(newStr[j]+''+newStr[j+newStr.length/2])
           if(str[i]==newStr[j]){
               stack.push(str[i]);
-              if(newStr[j]==newStr[j+newStr.length/2]){
-                  stack.pop();
-              }
-              
+             
           } 
-         if (str[i]!==newStr[j]){
-             for(let k=0;k<newStr.length/2;k++){
-               if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
-                  //alert(str[i-1]+''+str[i]); 
-                  stack.pop();
-               } 
-             }
-          }
-          if (str[i]==newStr[j+newStr.length/2]){
+         
+          if (str[i]!==newStr[j]){
              for(let k=0;k<newStr.length/2;k++){
                if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
                   //alert(str[i-1]+''+str[i]); 
@@ -54,5 +45,62 @@ module.exports = function check(str, bracketsConfig) {
     }
     else { return true;
         
+    }
+       
+    }
+   else{
+     let stack=[];
+    for (let i=0;i<str.length;i++){
+       for(let j=0;j<newStr.length/2;j++){
+           //alert(newStr[j]+''+newStr[j+newStr.length/2])
+          if(str[i]==newStr[j]){
+              stack.push(str[i]);
+              /*if(newStr[j]==newStr[j+newStr.length/2]){
+                  stack.pop();
+              }*/
+              
+          } 
+          if(str[i-1]==newStr[j+newStr.length/2] && str[i]==newStr[j]){
+                  
+                      //alert(str[i-1]+''+str[i]); 
+                      //return false;
+                  }
+         if (str[i]!==newStr[j]){
+             for(let k=0;k<newStr.length/2;k++){
+               if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
+                  //alert(str[i-1]+''+str[i]); 
+                  
+                  stack.pop();
+               } 
+             }
+          }
+          if (str[i]==newStr[j+newStr.length/2]){
+             for(let k=0;k<newStr.length/2;k++){
+               if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
+                  //alert(str[i-1]+''+str[i]); 
+                  
+                  stack.pop();
+               } 
+             }
+          }
+          if (str[i]==newStr[j+newStr.length/2]){
+             for(let k=0;k<newStr.length/2;k++){
+               if(str[i]==newStr[k+newStr.length/2] && str[i-1]==newStr[k]){
+                  //alert(str[i-1]+''+str[i]); 
+                  
+                  stack.pop();
+               } 
+             }
+          }
+       }
+    }
+    //alert(stack);
+    if(stack.length>0){
+        return false;
+    }
+    else { return true;
+        
+    }
+       
     }
 }
